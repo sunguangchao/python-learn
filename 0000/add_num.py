686bdce750bb2b6a4ed84192ec04dacac0e5bd79
sunguangchao <1198125767@qq.com>
2016-11-26 00:11:13 +0800

    0000 is not success
    Signed-off-by: sunguangchao <1198125767@qq.com>

--- 0000/add_num.py
+++ 0000/add_num.py
@@ -6,7 +6,7 @@ def add_num(picPath,num):
 	x,y = image.size
 	myfont = ImageFont.truetype('Futura.tff',x/3)
 	#加载一个truetype字体文件，并且创建一个对象
-	ImageDraw.Draw(img).text((2 * x / 3,0),str(num),front = myfont,fill = 'red')
+	ImageDraw.Draw(image).text((2 * x / 3,0),str(num),front = myfont,fill = 'red')
 	image.save('pic_with_num.jpg')
 if __name__ == '__main__':
-	add_num('abc.jpg',9)
+	add_num('0000.jpg',9)
